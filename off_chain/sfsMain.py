494fa30a769ece8b64f33ecb8b2c468191c15e4e
domenicolaporta00 <s1095492@studenti.univpm.it>
2025-03-07 15:31:26 +0100

    Commit di prova

--- off_chain/sfsMain.py
+++ off_chain/sfsMain.py
@@ -6,7 +6,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QSplashScreen
 
 from view.vista_accedi import VistaAccedi
-
+# Prova
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
