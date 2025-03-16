d8a12b9529770ad68759eacea88ac1a87be9caee
Bxster-Uni <s1098762@studenti.univpm.it>
2025-03-16 18:15:04 +0100

    tolta verifica opt su registrazione/login

--- off_chain/controllers/controller_autenticazione.py
+++ off_chain/controllers/controller_autenticazione.py
@@ -1,7 +1,6 @@
 from sqlite3 import IntegrityError
 
 import bcrypt
-import pyotp
 
 from database_domenico.db_login import DatabaseLogin, UniqueConstraintError, DatabaseError, \
     PasswordTooShortError, PasswordWeakError
@@ -15,14 +14,11 @@ class ControllerAutenticazione:
     def registrazione(self, username, password, tipo, indirizzo):
         """Tenta di aggiungere un utente, gestendo eventuali errori."""
         try:
-            # Genera una chiave segreta per l'autenticazione a due fattori
-            secret_key = pyotp.random_base32()
+            # Inserisce le credenziali nel database (senza chiave segreta OTP)
+            self.database.inserisci_credenziali_e_azienda(username, password, tipo, indirizzo, secret_key=None)
 
-            # Inserisce le credenziali e la chiave segreta nel database
-            self.database.inserisci_credenziali_e_azienda(username, password, tipo, indirizzo, secret_key)
-
-            # Restituisce il successo insieme alla chiave segreta
-            return True, "Utente registrato con successo!", secret_key
+            # Restituisce il successo
+            return True, "Utente registrato con successo!", None
         except PasswordTooShortError as e:
             return False, str(e), None
         except PasswordWeakError as e:
@@ -32,7 +28,7 @@ class ControllerAutenticazione:
         except DatabaseError:
             return False, "Errore nel database.", None
 
-    def login(self, username, password, otp_code):
+    def login(self, username, password, otp_code=None):
         # Recupera le credenziali dell'utente specifico
         credenziale = self.database.get_credenziale_by_username(username)
 
@@ -41,20 +37,9 @@ class ControllerAutenticazione:
 
         id_ = credenziale[0]
         password_hash = credenziale[2]  # La password hashata salvata nel database
-        secret_key = credenziale[3]  # Chiave segreta per OTP
-
-        # if not bcrypt.checkpw(password.encode('utf-8'), password_hash):
-        #     return None, "Password errata!"  # Password errata
 
         if password != password_hash:
             return None, "Password errata!"
-
-        # **Verifica OTP (se richiesto)**
-        # if otp_code:
-        #     totp = pyotp.TOTP(secret_key)
-        #     if not totp.verify(otp_code):  # Controllo OTP
-        #         print('Errore OTP')
-        #         return None  # OTP errato
 
         # Recupera l'azienda associata
         azienda = self.database.get_azienda_by_id(id_)
